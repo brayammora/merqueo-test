d3ec4372c3e825471dac33f162ba8e1784e337d5
Brayam Alberto Mora Arias - Ceiba Software <brayam.mora@ceiba.com.co>
2021-10-16 11:31:31 -0500

    Creating project and first module folder structure

---
+++ merqueo-testUITests/merqueo_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  merqueo_testUITestsLaunchTests.swift
+//  merqueo-testUITests
+//
+//  Created by Brayam Alberto Mora Arias - Ceiba Software on 15/10/21.
+//
+
+import XCTest
+
+class merqueo_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
